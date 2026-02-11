8e2364c0db86f0f74027ebf815250bb2ab7331c3
shamiivan <shamiivan@gmail.com>
2026-02-11 07:40:23 -0500

    feat: integrate new design system and components into main pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import client from "../../tina/__generated__/client";
-import ClientPage from "./client-page";
+import HomePage from "../components/pages/home-page";
 
 export default async function Home() {
   const { data, query, variables } = await client.queries.page({
     relativePath: "home.md"
   });
 
-  return <ClientPage data={data} query={query} variables={variables} />;
+  return <HomePage data={data} query={query} variables={variables} />;
 }
